8973046385700ebca828988bc21e0ad38a070277
Nikoleta Verbeck <nerdynick@gmail.com>
2020-06-09 16:40:27 -0600

    Change up logic on EBS volume to better deal with 0 server counts

--- modules/base_node/main.tf
+++ modules/base_node/main.tf
@@ -1,5 +1,5 @@
 locals {
-    volumes_to_mount = setproduct(aws_instance.instance, var.ebs_volumes)
+    volumes_to_mount = setproduct(range(var.servers), var.ebs_volumes)
 }
 data "template_file" "node_name" {
     count           = var.servers
@@ -44,13 +44,13 @@ resource "aws_instance" "instance" {
 resource "aws_ebs_volume" "instance_volume" {
     count = length(local.volumes_to_mount)
     
-    availability_zone = local.volumes_to_mount[count.index][0].availability_zone
+    availability_zone = aws_instance.instance[local.volumes_to_mount[count.index][0]].availability_zone
     encrypted = local.volumes_to_mount[count.index][1].encrypted
     size = local.volumes_to_mount[count.index][1].size
     type = local.volumes_to_mount[count.index][1].type
     kms_key_id = local.volumes_to_mount[count.index][1].kms_key_id
     tags = merge(var.tags, local.volumes_to_mount[count.index][1].tags, {
-        Name:  "${local.volumes_to_mount[count.index][0].tags["Name"]}-${local.volumes_to_mount[count.index][1].name}"
+        Name:  "${aws_instance.instance[local.volumes_to_mount[count.index][0]].tags["Name"]}-${local.volumes_to_mount[count.index][1].name}"
     })
 }
 
@@ -59,7 +59,7 @@ resource "aws_volume_attachment" "instance_volume_attach" {
     
     device_name = local.volumes_to_mount[count.index][1].device_name
     volume_id = aws_ebs_volume.instance_volume[count.index].id
-    instance_id = local.volumes_to_mount[count.index][0].id
+    instance_id = aws_instance.instance[local.volumes_to_mount[count.index][0]].id
 }
 
 resource "aws_route53_record" "dns_record" {
